1105856a2796fd8c821578fee12533867da9ab72
Eyke Mesquita <eyke1313@gmail.com>
2023-08-23 17:22:50 -0300

    projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,5 @@
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.print("Eu sou um Projeto de Conta");
     }
 }
